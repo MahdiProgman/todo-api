578272918d1835b06c50c9dfa129a243fdd2d42f
Mahdi Habibkhah <mahdihabibkah78@gmail.com>
2024-11-14 11:38:02 +0330

    chore: created logging system file in src/config

---
+++ src/config/logger.ts
@@ -0,0 +1,48 @@
+import pino, { destination } from 'pino';
+import * as path from 'path';
+import * as fs from 'fs';
+import PinoHttp from 'pino-http';
+
+const logDir = path.join(__dirname, '../../logs');
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const logger = pino({
+  level: 'info',
+  transport: {
+    targets: [
+      {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+          ignore: 'pid,hostname',
+        },
+      },
+      {
+        target: 'pino/file',
+        options: {
+          destination: path.join(logDir, 'app.log'),
+          translateTime: 'SYS:standard',
+        },
+      },
+    ],
+  },
+});
+
+const errorLogger = pino({
+  level: 'error',
+  transport: {
+    target: 'pino/file',
+    options: {
+      destination: path.join(logDir, 'error.log'),
+    },
+  },
+});
+
+const loggerMiddleware = PinoHttp({
+  logger: logger,
+});
+
+export { logger, errorLogger, loggerMiddleware };
