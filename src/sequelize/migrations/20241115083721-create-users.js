bd0285e9714b2888a1bee0913390ab4ed53c2261
Mahdi Habibkhah <mahdihabibkah78@gmail.com>
2024-11-15 12:39:58 +0330

    created migration for 'users' table

---
+++ src/sequelize/migrations/20241115083721-create-users.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      join_date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+
+    await queryInterface.addIndex('users', ['email'], {
+      name: 'idx_email',
+      unique: true,
+    });
+    await queryInterface.addIndex('users', ['username'], {
+      name: 'idx_username',
+      unique: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeIndex('users', 'idx_email');
+    await queryInterface.removeIndex('users', 'idx_username');
+
+    await queryInterface.dropTable('users');
+  },
+};
