aede4518a0cf8cd7fa31b7f22a2bbe5ba73b9c2a
Mahdi Habibkhah <mahdihabibkah78@gmail.com>
2024-11-15 12:35:12 +0330

    chore: defined 'SequelizeConfig' and used it in src/config/config.ts in DB_CONFIG

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,18 @@
 import config from 'config';
-import { ApiConfig } from '../types/config';
+import { ApiConfig, SequelizeConfig } from '../types/config';
+
+const dbConfig: SequelizeConfig = config.get<SequelizeConfig>('db');
 
 export const API: ApiConfig = {
   PORT: config.get<number>('api.port'),
+};
+export const DB_CONFIG: SequelizeConfig = {
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
+  host: dbConfig.host,
+  dialect: dbConfig.dialect,
+  port: dbConfig.port,
+  pool: dbConfig.pool,
+  logging : dbConfig.logging
 };
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,3 +1,21 @@
+import { Dialect } from 'sequelize';
+
 export interface ApiConfig {
   PORT: number;
+}
+
+export interface SequelizeConfig {
+  username: string;
+  password: string;
+  database: string;
+  host: string;
+  dialect: Dialect;
+  port: number;
+  pool : {
+    max: number;
+    min: number;
+    acquire: number;
+    idle: number;
+  },
+  logging : boolean;
 }
