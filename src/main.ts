436aff9b31355033918c312976f9a6a2aa5acac3
Mahdi Habibkhah <mahdihabibkah78@gmail.com>
2024-11-15 12:37:33 +0330

    chore: added connectToDB func into src/main.ts for DB Connection

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import api from './app/api';
 import http from 'http';
 import { API } from './config/config';
 import { logger } from './config/logger';
+import { connectToDB } from './config/db';
 
 const server: http.Server = http.createServer(api);
 
-server.listen(API.PORT, () => {
+server.listen(API.PORT, async () => {
   logger.info(`Server Is Listening On Port ${API.PORT}`);
+  await connectToDB();
 });
