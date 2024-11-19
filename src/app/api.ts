a002072f48f842135c3efb660b71b3bb765e88e7
Mahdi Habibkhah <mahdihabibkah78@gmail.com>
2024-11-19 21:28:42 +0330

    chore: enable cookies and used router in src/app/api.ts

--- src/app/api.ts
+++ src/app/api.ts
@@ -4,10 +4,16 @@ import cors from 'cors';
 import helmetConfig from '../config/helmet';
 import helmet from 'helmet';
 import { loggerMiddleware } from '../config/logger';
+import cookieParser from 'cookie-parser';
+import apiRouter from './router';
 
 const api = express();
 
-api.use(cors());
+api.use(cors({
+  credentials : true
+}));
+
+api.use(cookieParser());
 
 api.use(loggerMiddleware);
 
@@ -20,5 +26,7 @@ api.get('/', (req: Request, res: Response) => {
     `<h1>API Is Listening On Port <code style="background-color: black; color: white;">${API.PORT}</code></h1>`,
   );
 });
+
+api.use('/api', apiRouter);
 
 export default api;
