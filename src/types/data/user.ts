a0c72667550568a46846fb5cbd714b06c720df91
Mahdi Habibkhah <mahdihabibkah78@gmail.com>
2024-11-19 21:54:39 +0330

    chore: created type for refresh_token data

--- src/types/data/user.ts
+++ src/types/data/user.ts
@@ -1,3 +1,5 @@
+import { JwtPayload } from "jsonwebtoken";
+
 export interface UserRegistration {
     first_name : string;
     last_name : string;
@@ -9,4 +11,9 @@ export interface UserRegistration {
 export interface LoginUserData {
     email : string;
     password : string;
+}
+
+export interface RefreshTokenData extends JwtPayload {
+    userId : string;
+    version : number;
 }
